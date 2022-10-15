8f25dbd476c9bfa5b86dc96a6b69fd3bca0f5b0e
Goekhan Oezdemir <goekhan.oezdemir90@googlemail.com>
2022-10-15 19:38:55 +0200

    first commit

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,11 @@ public class Hello {
     public static void main(String[]args){
          System.out.println("Hello Gökhan");
 
-         int myFirstNumber = (5*20)+(10*30);
-         System.out.println(myFirstNumber);
+         int myFirstNumber = 5;
+         int mySecondNumber = 12;
+         int myThirdNumber = myFirstNumber*2;
+         int myTotal = myFirstNumber+mySecondNumber+myThirdNumber;
+         int myLastOne = 1000;
+         System.out.println(myLastOne-myTotal);
     }
 }
