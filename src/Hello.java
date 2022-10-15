75d793d67e5688254ecf3fb95d34bac4a1c319bf
Goekhan Oezdemir <goekhan.oezdemir90@googlemail.com>
2022-10-15 19:18:27 +0200

    First Steps

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+
+    public static void main(String[]args){
+         System.out.println("Hello Gökhan");
+
+         int myFirstNumber = (5*20)+(10*30);
+         System.out.println(myFirstNumber);
+    }
+}
